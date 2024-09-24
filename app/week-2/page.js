6148c3da58c2c4a0adcafedc4f7e87b8bc241804
Tokorio <131092204+Tokorio@users.noreply.github.com>
2024-09-24 16:51:07 -0600

    Finished 2nd week assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+
+function Page() {
+    return (
+      <main>
+        <h1>Shopping List</h1>
+        <StudentInfo />
+      </main>
+    );
+  }
+
+export default Page;
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <p>Martin Davis</p>
+            <Link href="https://github.com/Tokorio/cprg306-assignments">https://github.com/</Link>
+        </main>
+    );
+}
